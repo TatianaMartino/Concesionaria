1c2cd61ac5158e17801cf074c9a699cb1b08628a
TatianaMartino <brichumelli@gmail.com>
2025-04-26 14:29:40 -0300

    Se agrega carpeta controller de cliente

---
+++ src/main/java/com/sample/core/controller/cliente/ClienteController.java
@@ -0,0 +1,12 @@
+package com.sample.core.controller.cliente;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/GuardarDatosCliente")
+
+public class ClienteController extends HttpServlet {
+	
+	
+
+}
