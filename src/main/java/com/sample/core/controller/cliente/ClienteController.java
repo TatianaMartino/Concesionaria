bff0a7a9e93aab773cb1129ca0732eb2a4201be3
Priscilaa07 <prisciladelsolar123@gmail.com>
2025-04-30 10:17:39 -0300

    Se crea controller

--- src/main/java/com/sample/core/controller/cliente/ClienteController.java
+++ src/main/java/com/sample/core/controller/cliente/ClienteController.java
@@ -1,12 +1,50 @@
 package com.sample.core.controller.cliente;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sample.core.service.cliente.ClienteService;
+import com.sample.core.service.cliente.ClienteServiceImp;
 
 @WebServlet(urlPatterns = "/GuardarDatosCliente")
 
 public class ClienteController extends HttpServlet {
-	
-	
+
+	ClienteService clienteService = new ClienteServiceImp();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		 
+
+		String nombre = req.getParameter("nombre");
+		String apellido = req.getParameter("apellido");
+		String correo = req.getParameter("correo");
+		String dni = req.getParameter("dni");
+		String codigo_postal = req.getParameter("codigo_postal");
+		String dato_tarjeta = req.getParameter("dato_tarjeta");
+		String telefono = req.getParameter("telefono");
+
+		// Ver datos recibidos del formulario
+		System.out.println("nombre: " + nombre);
+		System.out.println("apellido: " + apellido);
+		System.out.println("correo: " + correo);
+		System.out.println("dni: " + dni);
+		System.out.println("codigo_postal: " + codigo_postal);
+		System.out.println("dato_tarjeta: " + correo);
+		System.out.println("telefono: " + telefono);
+
+		try {
+			clienteService.guardar_datos(nombre, apellido, correo, dni, codigo_postal, dato_tarjeta, telefono);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
