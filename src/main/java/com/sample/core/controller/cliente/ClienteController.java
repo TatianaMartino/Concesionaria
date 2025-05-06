28046cb9c1e020c864a64fcc4b6dbf85a22ac3d6
TatianaMartino <tatianamartino90@gmail.com>
2025-05-05 22:11:50 -0300

    Actualizo Cliente Controller

--- src/main/java/com/sample/core/controller/cliente/ClienteController.java
+++ src/main/java/com/sample/core/controller/cliente/ClienteController.java
@@ -8,19 +8,44 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sample.core.service.AutoService;
+import com.sample.core.service.AutoServiceImpl;
 import com.sample.core.service.cliente.ClienteService;
 import com.sample.core.service.cliente.ClienteServiceImp;
+
+import domain.Auto;
+import domain.Pedido;
+import utils.Disponibilidad;
 
 @WebServlet(urlPatterns = "/GuardarDatosCliente")
 
 public class ClienteController extends HttpServlet {
 
 	ClienteService clienteService = new ClienteServiceImp();
+	AutoService autoService = new AutoServiceImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		 
+		  
+				Auto auto = null;
 
+				Pedido pedido; //Generamos un pedido
+				
+			   String idAuto = req.getParameter("id");
+				int idAUtoParseado = Integer.parseInt(idAuto);
+				
+				
+					try {
+					 auto =	autoService.consultarAuto(idAUtoParseado); //para consultar el id del auto que esta reservando el cliente
+					 pedido = new Pedido(auto, Disponibilidad.RESERVADO);
+					 
+					 
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			
+			   
 		String nombre = req.getParameter("nombre");
 		String apellido = req.getParameter("apellido");
 		String correo = req.getParameter("correo");
@@ -29,6 +54,7 @@ public class ClienteController extends HttpServlet {
 		String dato_tarjeta = req.getParameter("dato_tarjeta");
 		String telefono = req.getParameter("telefono");
 
+		
 		// Ver datos recibidos del formulario
 		System.out.println("nombre: " + nombre);
 		System.out.println("apellido: " + apellido);
