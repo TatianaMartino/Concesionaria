c8ac37904c4f0e329ba6cacd2e68ec0cff696cd5
TatianaMartino <tatianamartino90@gmail.com>
2025-05-20 10:56:48 -0300

    agrego los datos del cliente

--- src/main/java/domain/Pedido.java
+++ src/main/java/domain/Pedido.java
@@ -1,31 +1,55 @@
 package domain;
 
 import utils.Disponibilidad;
+import utils.Situacion;
 
 public class Pedido {
+
+	private String id;
+	private int monto;
+
+	private Situacion situacion; // enum de Situacion
 
 	Auto auto;
 
 	Disponibilidad disponibilidad;
 
-	public Pedido(Auto auto, Disponibilidad disponibilidad) {
+	Cliente cliente;
+
+	public Pedido(Auto auto, Disponibilidad disponibilidad, Cliente cliente, Situacion situacion) {
 		super();
 		this.auto = auto;
 		this.disponibilidad = disponibilidad;
-
-		// obtener datos cliente
-		/*
-		 * String nombre = req.getParameter("nombre"); String apellido =
-		 * req.getParameter("apellido"); String correo = req.getParameter("correo");
-		 * String dni = req.getParameter("dni"); String codigo_postal =
-		 * req.getParameter("codigo_postal"); String dato_tarjeta =
-		 * req.getParameter("dato_tarjeta"); String telefono =
-		 * req.getParameter("telefono");
-		 */
+		this.cliente = cliente; // obtener datos del cliente
+		this.situacion = situacion; // obtener estado del pago
 	}
 
 	public String generarCodigo() {
 
 		return "abcdefg";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getMonto() {
+		return monto;
+	}
+
+	public void setMonto(int monto) {
+		this.monto = monto;
+	}
+
+	public Situacion getSituacion() {
+		return situacion;
+	}
+
+	public void setSituacion(Situacion situacion) {
+		this.situacion = situacion;
 	}
 }
