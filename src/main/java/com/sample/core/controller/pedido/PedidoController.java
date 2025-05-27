3250aa3761a35f67cb3b851dd23e8e38289c6f1e
TatianaMartino <tatianamartino90@gmail.com>
2025-05-27 16:39:12 -0300

    Agrego el controller de pedido (sin codigo)

---
+++ src/main/java/com/sample/core/controller/pedido/PedidoController.java
@@ -0,0 +1,19 @@
+package com.sample.core.controller.pedido;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PedidoController extends HttpServlet {
+
+public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	 // Obtener datos necesarios del formulario
+   // String clienteId = req.getParameter("clienteId");
+   // String idAuto = req.getParameter("idAuto");
+	
+}
+}
