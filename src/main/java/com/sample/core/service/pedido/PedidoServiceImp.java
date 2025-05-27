18609e12746ce14d9b5165f75742873212f3bc7e
TatianaMartino <tatianamartino90@gmail.com>
2025-05-27 16:43:07 -0300

    agrego la implementacion del service de pedido (sin codigo)

---
+++ src/main/java/com/sample/core/service/pedido/PedidoServiceImp.java
@@ -0,0 +1,19 @@
+package com.sample.core.service.pedido;
+
+import domain.Pedido;
+
+public class PedidoServiceImp implements PedidoService{
+
+	@Override
+	public void guardarPedido(Pedido pedido) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buscarPorId(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
