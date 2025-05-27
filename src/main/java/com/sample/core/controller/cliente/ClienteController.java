6e3126fe2d9ecaa1aa2304e9b9a0c5b5a77f6464
TatianaMartino <brichumelli@gmail.com>
2025-05-27 12:45:48 -0300

    Mejoro la creación del pedido

--- src/main/java/com/sample/core/controller/cliente/ClienteController.java
+++ src/main/java/com/sample/core/controller/cliente/ClienteController.java
@@ -14,8 +14,10 @@ import com.sample.core.service.cliente.ClienteService;
 import com.sample.core.service.cliente.ClienteServiceImp;
 
 import domain.Auto;
+import domain.Cliente;
 import domain.Pedido;
 import utils.Disponibilidad;
+import utils.SituacionPedidos;
 
 @WebServlet(urlPatterns = "/GuardarDatosCliente")
 
@@ -26,46 +28,36 @@ public class ClienteController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		  
-				Auto auto = null;
-
-				Pedido pedido; //Generamos un pedido
-				
-			   String idAuto = req.getParameter("id");
-				int idAUtoParseado = Integer.parseInt(idAuto);
-				
-				
-					try {
-					 auto =	autoService.consultarAuto(idAUtoParseado); //para consultar el id del auto que esta reservando el cliente
-					 pedido = new Pedido(auto, Disponibilidad.RESERVADO);
-					 
-					 
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			
-			   
-		String nombre = req.getParameter("nombre");
-		String apellido = req.getParameter("apellido");
-		String correo = req.getParameter("correo");
-		String dni = req.getParameter("dni");
-		String codigo_postal = req.getParameter("codigo_postal");
-		String dato_tarjeta = req.getParameter("dato_tarjeta");
-		String telefono = req.getParameter("telefono");
-
-		
-		// Ver datos recibidos del formulario
-		System.out.println("nombre: " + nombre);
-		System.out.println("apellido: " + apellido);
-		System.out.println("correo: " + correo);
-		System.out.println("dni: " + dni);
-		System.out.println("codigo_postal: " + codigo_postal);
-		System.out.println("dato_tarjeta: " + correo);
-		System.out.println("telefono: " + telefono);
 
 		try {
+			// Obtener datos del auto
+			String idAuto = req.getParameter("id");
+			int idAUtoParseado = Integer.parseInt(idAuto);
+			Auto auto = autoService.consultarAuto(idAUtoParseado); // para consultar el id del auto que esta reservando
+																	// el cliente
+
+			// Obtener datos del cliente
+			String nombre = req.getParameter("nombre");
+			String apellido = req.getParameter("apellido");
+			String correo = req.getParameter("correo");
+			String dni = req.getParameter("dni");
+			String codigo_postal = req.getParameter("codigo_postal");
+			String dato_tarjeta = req.getParameter("dato_tarjeta");
+			String telefono = req.getParameter("telefono");
+
+			//Crear cliente y guardar los datos
+			Cliente cliente = new Cliente(nombre, apellido, correo, dni, codigo_postal, dato_tarjeta, telefono);
+			
 			clienteService.guardar_datos(nombre, apellido, correo, dni, codigo_postal, dato_tarjeta, telefono);
+			
+			//Crear Pedido
+			Pedido pedido = new Pedido(auto, Disponibilidad.RESERVADO, cliente, SituacionPedidos.PAGOPROVISORIO);
+
+		// Ver datos recibidos del formulario
+		System.out.println("Datos del Pedido");
+		System.out.println("Cliente: " + cliente.getNombre() + "" + cliente.getApellido());
+		System.out.println("Auto: " + auto.getMarca() + ""+ auto.getModelo());
+
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
