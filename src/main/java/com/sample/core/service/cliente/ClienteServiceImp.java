e39f13da1ff235dc78c04299adf3838011c2bac1
TatianaMartino <tatianamartino90@gmail.com>
2025-05-27 22:00:10 -0300

    Se modifico el cliente ServiceImp

--- src/main/java/com/sample/core/service/cliente/ClienteServiceImp.java
+++ src/main/java/com/sample/core/service/cliente/ClienteServiceImp.java
@@ -6,14 +6,12 @@ import com.sample.core.dao.cliente.ClienteDaoImp;
 public class ClienteServiceImp implements ClienteService{
 	
 	//
-	ClienteDao clientedao = new ClienteDaoImp(); 
+	ClienteDao clienteDao = new ClienteDaoImp(); 
 
 	@Override
-	public void guardar_datos(String nombre, String apellido, String correo, String dni, String codigo_postal,
-			String dato_tarjeta, String telefono) throws Exception {
-		
-		clientedao.guardar_datos(nombre, apellido, correo, dni, codigo_postal, dato_tarjeta, telefono);
-		
+	public void guardar_datos(String nombre, String apellido, String correo, String dni, String codigo_postal, String dato_tarjeta, String telefono) throws Exception {
+		clienteDao.guardar_datos(nombre, apellido, correo, dni, codigo_postal, dato_tarjeta, telefono);
+	
 	}
 	
 
