9ec8f934c0cb7e51309123f7295157ca6f02efba
briisaam <brichumelli@gmail.com>
2025-05-28 00:15:43 -0300

    Se modificó el DAO de cliente

--- src/main/java/com/sample/core/dao/cliente/ClienteDao.java
+++ src/main/java/com/sample/core/dao/cliente/ClienteDao.java
@@ -1,7 +1,15 @@
 package com.sample.core.dao.cliente;
 
+import java.util.List;
+
+import domain.Cliente;
+
 public interface ClienteDao {
 	
 	public void guardar_datos(String nombre, String apellido, String correo, String dni, String codigo_postal, String dato_tarjeta, String telefono) throws Exception;
+
+	public List<Cliente> listarCliente() throws Exception;
+	
+	public Cliente consultarCliente(int id) throws Exception;
 
 }
