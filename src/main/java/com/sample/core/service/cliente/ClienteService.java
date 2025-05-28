fffe58a5d59b3c10aa111d2665dd4188acb87330
briisaam <brichumelli@gmail.com>
2025-05-28 00:17:11 -0300

    Se modificó el service de Cliente

--- src/main/java/com/sample/core/service/cliente/ClienteService.java
+++ src/main/java/com/sample/core/service/cliente/ClienteService.java
@@ -1,8 +1,15 @@
 package com.sample.core.service.cliente;
 
-public interface ClienteService {
-	
-	public void guardar_datos(String nombre, String apellido, String correo, String dni, String codigo_postal, String dato_tarjeta, String telefono) throws Exception;
+import java.util.List;
 
+import domain.Cliente;
+
+public interface ClienteService {
+
+	public List<Cliente> listarCliente() throws Exception;
+
+	public Cliente consultarCliente(int id) throws Exception;
+
+	public void guardar_datos(String nombre, String apellido, String correo, String dni, String codigo_postal, String dato_tarjeta, String telefono) throws Exception;
 
 }
