5b845b5386553ea19f03482f8fe11e9839d630b3
TatianaMartino <tatianamartino90@gmail.com>
2025-05-27 21:58:52 -0300

    Se modificó el controller cliente

--- src/main/java/com/sample/core/controller/cliente/ClienteController.java
+++ src/main/java/com/sample/core/controller/cliente/ClienteController.java
@@ -27,7 +27,7 @@ public class ClienteController extends HttpServlet {
 	AutoService autoService = new AutoServiceImpl();
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		try {
 			// Obtener datos del auto
