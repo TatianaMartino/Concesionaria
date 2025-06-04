221317a86052d61d1617c3172de400fb7ae7689a
TatianaMartino <brichumelli@gmail.com>
2025-06-04 06:46:19 -0300

    Se modificó el controller cliente

--- src/main/java/com/sample/core/controller/cliente/ClienteController.java
+++ src/main/java/com/sample/core/controller/cliente/ClienteController.java
@@ -19,7 +19,6 @@ import domain.Pedido;
 import utils.Disponibilidad;
 import utils.SituacionPedidos;
 
-
 @WebServlet(urlPatterns = "/GuardarDatosCliente")
 
 public class ClienteController extends HttpServlet {
@@ -31,16 +30,16 @@ public class ClienteController extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		try {
-			
-			Cliente cliente = new Cliente();			
-			
+
+			Cliente cliente = new Cliente();
+
 			// Obtener datos del auto
 			String idAuto = req.getParameter("id");// aca esta el id del formulario
 
 			if (idAuto != null) {
 
 				int idAUtoParseado = Integer.parseInt(idAuto);
-				Auto auto = autoService.consultarAuto(idAUtoParseado); 
+				Auto auto = autoService.consultarAuto(idAUtoParseado);
 				// para consultar el id del auto que esta reservando el cliente
 
 				// Obtener datos del cliente
@@ -55,23 +54,32 @@ public class ClienteController extends HttpServlet {
 				// Crear cliente y guardar los datos del formulario
 				clienteService.guardar_datos(nombre, apellido, correo, dni, codigo_postal, dato_tarjeta, telefono);
 
+				// Se guardaan los datos del formulario en clase cliente
+				cliente.setNombre(nombre);
+				cliente.setApellido(apellido);
+				cliente.setCorreo(correo);
+				cliente.setDni(dni);
+				cliente.setCodigoPostal(codigo_postal);
+				cliente.setDatoTarjeta(dato_tarjeta);
+				cliente.setTelefono(telefono);
 
 				// Crear Pedido
 				Pedido pedido = new Pedido(auto, Disponibilidad.RESERVADO, cliente, SituacionPedidos.PAGOPROVISORIO);
-
+				
 				// Ver datos recibidos del formulario
 				System.out.println("Datos del Pedido");
 				System.out.println("Cliente: " + cliente.getNombre() + "" + cliente.getApellido());
 				System.out.println("Auto: " + auto.getMarca() + "" + auto.getModelo());
 
 			} else {
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID del auto no proporcionado."); //en caso de que el id este vacio
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID del auto no proporcionado."); // en caso de que el id este vacio
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-
 		}
+		
+		resp.sendRedirect(req.getContextPath() + "/home.jsp");  // Redirige al inicio
 
 	}
 }
