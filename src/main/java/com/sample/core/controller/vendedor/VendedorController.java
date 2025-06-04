3c2d9d5c1c32eb3cb37a161bc80eb76330b33150
TatianaMartino <brichumelli@gmail.com>
2025-06-03 23:20:58 -0300

    Controller vendedor

--- src/main/java/com/sample/core/controller/vendedor/VendedorController.java
+++ src/main/java/com/sample/core/controller/vendedor/VendedorController.java
@@ -14,8 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.sample.core.dao.config.Conexion;
-import com.sample.core.service.encargado.VendedorService;
-import com.sample.core.service.encargado.VendedorServiceImpl;
+import com.sample.core.service.vendedor.VendedorService;
+import com.sample.core.service.vendedor.VendedorServiceImpl;
 
 @WebServlet(urlPatterns = "/CrearEncargado")
 public class VendedorController extends HttpServlet { // hereda de HttpServlet, lo que le permite manejar solicitudes
