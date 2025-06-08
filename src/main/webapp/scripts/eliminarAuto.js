8f4134b5c42374f5b112614aa8c67cdabe864cae
briisaam <brichumelli@gmail.com>
2025-06-08 13:14:19 -0300

    Se agrega el SweetAlert

--- src/main/webapp/scripts/eliminarAuto.js
+++ src/main/webapp/scripts/eliminarAuto.js
@@ -1,21 +1,42 @@
 function myFunction(button) {
-    var autoId = $(button).data('id');
-    console.log('Id del auto a eliminar:', autoId); 
+	var autoId = $(button).data('id');
+	console.log('Id del auto a eliminar:', autoId);
 
-    $.ajax({
-        url: contextPath + '/AutoEliminar', 
-        type: 'GET',
-        data: { id: autoId }, 
-        success: function(response) {
-            console.log('Auto eliminado:', response);
-            
+	//agregamos sweet alert
+	Swal.fire({
+		title: "Esta seguro que desea eliminar el Auto?",
+		text: "El auto sera eliminado!",
+		icon: "warning",
+		showCancelButton: true,
+		confirmButtonColor: "#3085d6",
+		cancelButtonColor: "#d33",
+		confirmButtonText: "Si, estoy seguro!"
+	}).then((result) => {
+		if (result.isConfirmed) {
+			//agregamos ajax
+			$.ajax({
+				type: "get",
+				url: contextPath + "/AutoEliminar?id=" + autoId,
+				dataType: "json",
+				success: function(response) {
+					//agregamos sweet alert
+					Swal.fire({
+						title: "Eliminado!",
+						text: "El auto se elimino correctamente.",
+						icon: "success",
+						showConfirmButton: true,
+					}).then(response => {
+						if (response.isConfirmed) {
+							location.reload();
+						}
+					})
+					//fin sweet alert
 
-            if (response.estatus === 'ok') {
-                location.reload(); 
-            }
-        },
-        error: function(error) {
-            console.log('Error al eliminar el auto:', error);
-        }
-    });
+
+				}
+			});
+			//fin ajax
+
+		}
+	});
 }
