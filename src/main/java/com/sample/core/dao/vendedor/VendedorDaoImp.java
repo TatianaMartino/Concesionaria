9576c90e11f0c530e1daab06696b4f0173e504f6
Brisa Irina <brichumelli@gmail.com>
2025-06-11 23:50:30 -0300

    Update VendedorDaoImp.java

--- src/main/java/com/sample/core/dao/vendedor/VendedorDaoImp.java
+++ src/main/java/com/sample/core/dao/vendedor/VendedorDaoImp.java
@@ -14,48 +14,14 @@ import com.sample.core.exceptions.ErrorException;
 public class VendedorDaoImp implements VendedorDao {
 	private Conexion conexion = Conexion.getInstance();
 
-	private static final String queryList = "SELECT id,nombre,apellido,usuario,contrasena,correo, id_sucursal FROM Encargado";
+	private static final String queryAddEncargado = "INSERT INTO vendedor (nombre, apellido, usuario, password, correo, id_sucursal) VALUES (?,?,?,?,?,?)";
 
-	private static final String queryAddEncargado = "INSERT INTO Encargado (nombre, apellido, usuario, contrasena, correo, id_sucursal) VALUES (?,?,?,?,?,?)";
+	private static final String queryFindByUser = "SELECT id, usuario, password FROM vendedor where usuario = ?";
 
-	private static final String queryFindByUser = "SELECT id, usuario, password FROM encargado where usuario = ?";
-
-	private static final String queryFindByUserAndPassword = "SELECT id, usuario, password FROM encargado where usuario = ? and password = ?";
+	private static final String queryFindByUserAndPassword = "SELECT id, usuario, password FROM vendedor where usuario = ? and password = ?";
 	
-	private static final String queryConsultarUsuario = "SELECT id, usuario, contrasena FROM encargado where id=?";
+	private static final String queryConsultarUsuario = "SELECT id, usuario, password FROM vendedor where id=?";
 	
-	
-	@Override
-	public List<Vendedor> list() throws Exception {
-		ResultSet rs = null;
-		List<Vendedor> encargados = null;
-		Vendedor encargado = null;
-		PreparedStatement st = null;
-		try {
-			st = conexion.dameConnection().prepareStatement(queryList);
-			rs = st.executeQuery();
-			encargados = new ArrayList<Vendedor>();
-			while (rs.next()) {
-				encargado = new Vendedor(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
-						rs.getString(5), rs.getString(6), rs.getInt(7));
-				encargados.add(encargado);
-			}
-
-		} catch (Exception e) {
-			throw new ErrorException("Hubo un error al realizar la consulta", e);
-		} finally {
-			try {
-				st.close();
-				rs.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-
-		return encargados;
-	}
 
 	@Override
 	public void save(String nombre, String apellido, String usuario, String contra, String correo, int idSucursal)
