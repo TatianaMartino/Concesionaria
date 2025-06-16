bd8c911903933106a83c441b9b286aef7e9d0306
bririnaa <brisamartino656@gmail.com>
2025-06-16 11:39:47 -0300

    Se agrega dao traslado (EN PROCESO)

---
+++ src/main/java/com/sample/core/dao/reserva/ReservaDao.java
@@ -0,0 +1,5 @@
+package com.sample.core.dao.reserva;
+
+public interface ReservaDao {
+
+}
---
+++ src/main/java/com/sample/core/dao/reserva/ReservaDaoImp.java
@@ -0,0 +1,5 @@
+package com.sample.core.dao.reserva;
+
+public class ReservaDaoImp implements ReservaDao{
+
+}
