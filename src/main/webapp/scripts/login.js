8f979b5ec57a69980fee56ec41726cbf7f75dd7e
briisaam <brichumelli@gmail.com>
2025-06-18 00:34:45 -0300

    se añade sweet alert

--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -3,16 +3,33 @@ $(function() {
 		e.preventDefault();
 		var usuario = $("#usuario").val();
 		var password = $("#password").val();
+
 		$.ajax({
 			url: contextPath + '/Ingresar',
 			dataType: 'json',
 			success: function(data) {
-				alert(data.mensaje);
-				window.location.href = contextPath + '/LeerAuto';
+				Swal.fire({
+					title: 'Registrado con exito!',
+					text: 'El usuario se ha registrado exitosamente',
+					icon: 'success',
+					position: 'top-end',
+					showConfirmButton: false,
+					timer: 1500
+				});
+
+				setTimeout(function() {
+					window.location.href = contextPath + '/LeerAuto';
+				}, 1500);
 			},
-			error: function(data) {
-				alert(data.responseJSON.mensaje);
+
+			error: function(xhr, status, error) {
+				Swal.fire({
+					title: 'Error',
+					text: 'No se pudo registrar el usuario',
+					icon: 'error'
+				});
 			},
+
 			data: {
 				usuario: usuario,
 				password: password,
